fed481ca21a6cd1b02bc1e1d2e51ef604a3ca484
CanLuke <lukuan1992@sina.com>
2016-10-25 09:55:44 +0800

    修改了部分代码，使可以保存用户名

--- app/src/main/java/com/administrator/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/administrator/sharedpreferences/MainActivity.java
@@ -17,27 +17,27 @@ public class MainActivity extends Activity {
     private Button btnLogin;
     private Button btnCancle;
     private String user,pass;
-    private boolean state;
+    private SharedPreferences spf;
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         edtUserName = (EditText) findViewById(R.id.edit_user);
         edtPwd = (EditText) findViewById(R.id.edit_pwd);
-
         checkBox = (CheckBox) findViewById(R.id.checkbox);
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnCancle = (Button) findViewById(R.id.btn_cancle);
-        checkBox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (((CheckBox)v).isChecked()){
-                    state = true;
-                }else {
-                    state = false;
-                }
-            }
-        });
+        spf = getSharedPreferences("MyPre",MODE_PRIVATE);
+        editor = spf.edit();
+        String name = spf.getString("userName","");
+        if (name == null){
+            checkBox.setChecked(false);
+        }else{
+            checkBox.setChecked(true);
+            edtUserName.setText(name);
+        }
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,23 +52,15 @@ public class MainActivity extends Activity {
     private void login(String userName,String pwd) {
         user = userName;
         pass = pwd;
-        if (state){
-            SharedPreferences spf = getSharedPreferences("MyPre",MODE_PRIVATE);
-            SharedPreferences.Editor editor = spf.edit();
+        if (checkBox.isChecked()){
+
             editor.putString("userName",user);
             editor.putString("pwd",pass);
             editor.commit();
-            String name = spf.getString("userName", "");
-            if (name==null) {
-                checkBox.setChecked(false);
-            }else {
-                checkBox.setChecked(true);
-                edtUserName.setText(name);
-            }
-            /*System.out.println(spf.getString("userName", ""));
-            System.out.println(spf.getString("pwd", ""));*/
+
         }else {
-            
+            editor.remove("userName");
+            editor.commit();
         }
     }
 }
